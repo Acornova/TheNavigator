7b215ca7942eb8db292eaa6a9a6a481f609f3d80
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2025-10-20 22:10:45 +0300

    Update stuff

--- The Navigator/composeApp/src/commonMain/kotlin/com/acornova/thenavigator/App.kt
+++ The Navigator/composeApp/src/commonMain/kotlin/com/acornova/thenavigator/App.kt
@@ -1,26 +1,13 @@
 package com.acornova.thenavigator
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import thenavigator.composeapp.generated.resources.Res
-import thenavigator.composeapp.generated.resources.compose_multiplatform
-import thenavigator.composeapp.generated.resources.icon
-
 @Composable
 @Preview
 fun App() {
@@ -31,7 +18,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            
+
         }
     }
 }
